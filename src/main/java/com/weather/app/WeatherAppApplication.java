6b491e2766d9ca9a35d26ba033e6d710746e4872
Shubham Kumar <44669879+shubhamkumartyagi@users.noreply.github.com>
2018-11-05 15:39:55 +0530

    changed to webapp config

--- src/main/java/com/weather/app/WeatherAppApplication.java
+++ src/main/java/com/weather/app/WeatherAppApplication.java
@@ -2,18 +2,26 @@ package com.weather.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
-
 /**
- * This is spring boot application main file to bootstrap the weather application.
+ * This is spring boot application main file to bootstrap the weather
+ * application.
+ * 
  * @author shubham kumar
  *
  */
 @SpringBootApplication
 @ComponentScan("com.weather.*")
-public class WeatherAppApplication {
-	
+public class WeatherAppApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WeatherAppApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherAppApplication.class, args);
 	}
